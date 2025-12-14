677cd9fda36cd98991f7d94338e2cca388736d77
jin573 <rlawlstjs5573@gmail.com>
2025-12-14 20:01:28 +0900

    feat: add mutex to protect client list

--- client_list.c
+++ client_list.c
@@ -3,8 +3,11 @@
 #include <stdlib.h>
 #include "./client_list.h"
 
+pthread_mutex_t client_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 //insert
 void insert_client(ClientList* client_list, PlayerView* client_info){
+	pthread_mutex_lock(&client_list_mutex);
 	//clientNode dynamic 
 	ClientNode* client_node = (ClientNode*)malloc(sizeof(ClientNode));
 
@@ -13,24 +16,28 @@ void insert_client(ClientList* client_list, PlayerView* client_info){
 
 	client_list->head = client_node;
 	client_list->count++;
- 
+ 	pthread_mutex_unlock(&client_list_mutex);
 }
 
 //search
 PlayerView* search_client(ClientList* client_list, int client_id){
+	pthread_mutex_lock(&client_list_mutex);
 	ClientNode* current = client_list->head;
 
 	while(current){
 		if(current->data->client_id == client_id){
-			return current->data;
+			PlayerView* p = current->data;
+			pthread_mutex_unlock(&client_list_mutex);
+			return p;
 		}
 		current = current->next;
 	}
-
+	pthread_mutex_unlock(&client_list_mutex);
 	return NULL;
 }
 //remove
 void remove_client(ClientList* client_list, int client_id){
+	pthread_mutex_lock(&client_list_mutex);
 	ClientNode* prev = NULL;
 	ClientNode* current = client_list->head;
 
@@ -43,28 +50,31 @@ void remove_client(ClientList* client_list, int client_id){
 			}
 			free(current);
 			client_list->count--;
+			pthread_mutex_unlock(&client_list_mutex);
+			
 			printf("[Server] Client %d removed\n", client_id);
 			//test
 			print_clients(client_list);
+			
 			return;
 		}
 
 		prev = current;
 		current = current->next;
 	}
-
+	pthread_mutex_unlock(&client_list_mutex);
 }
 
 //print
 void print_clients(ClientList* client_list){
-
+	pthread_mutex_lock(&client_list_mutex);
 	for(ClientNode* client_node = client_list->head; 
 		client_node!=NULL;
 		client_node = client_node->next){
 			
 	printf("Client Name: %s\n", client_node->data->nick);
 	}
-
+	pthread_mutex_unlock(&client_list_mutex);
 }
 
 //clear
